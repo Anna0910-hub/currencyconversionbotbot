5e1c1d7138f94bee96df8f2d491ce313f025065a
Anna0910-hub <127149817+Anna0910-hub@users.noreply.github.com>
2023-05-06 00:12:57 +0300

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,12 @@
+TOKEN = '6108023610:AAG_iY-3375XQi5MxJWUUVBHsqBCsddobWc'
+
+
+exchanges = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'драм': 'AMD',
+    'риал': 'IRR',
+    'лира': 'TRY',
+    'лари': 'GEL'
+}
